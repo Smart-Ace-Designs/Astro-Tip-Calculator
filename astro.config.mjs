cd332193ad0976305d09677ba1ded33a5c168a9e
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-02-17 10:08:05 -0600

    Adds Vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [],
+  integrations: [vue()],
 });
