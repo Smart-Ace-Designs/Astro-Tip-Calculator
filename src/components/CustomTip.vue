c2a69d65d3460bf5e9673b440d43a11a10766350
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-02-19 20:02:49 -0600

    Refactors Vue components

---
+++ src/components/CustomTip.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const props = defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits(["update:model-value"]);
+const inputValue = ref(props.modelValue);
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    inputValue.value = newValue;
+  },
+);
+
+const updateValue = (event: Event) => {
+  emit("update:model-value", (event.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <input
+    :value="inputValue"
+    @input="updateValue"
+    type="text"
+    placeholder="Custom"
+    id="custom"
+    class="bg-theme-very-light-grayish-cyan text-theme-very-dark-cyan placeholder:text-theme-dark-grayish-cyan focus:bg-theme-very-light-grayish-cyan focus:outline-theme-strong-cyan caret-theme-strong-cyan rounded p-2 text-center text-2xl hover:cursor-pointer focus:text-right focus:placeholder:opacity-0"
+  />
+</template>
--- src/components/TipSelector.vue
+++ src/components/TipSelector.vue
@@ -2,9 +2,11 @@
 import { computed, ref } from "vue";
 
 import StandardTip from "./StandardTip.vue";
+import CustomTip from "./CustomTip.vue";
 
 const standardTip = ref<string>("");
 const customTip = ref<string>("");
+const standardTipsTypes = ["5", "10", "15", "25", "50"];
 
 const clearCustomTip = () => {
   customTip.value = "";
@@ -22,37 +24,13 @@ const tip = computed(() => {
     <p class="text-theme-dark-grayish-cyan mb-4">Select Tip %</p>
     <div class="grid grid-cols-3 gap-4" id="tip-selector">
       <StandardTip
-        tip="5"
+        v-for="type in standardTipsTypes"
+        :key="type"
+        :tip="type"
         v-model="standardTip"
         @clear-custom-tip="clearCustomTip"
       />
-      <StandardTip
-        tip="10"
-        v-model="standardTip"
-        @clear-custom-tip="clearCustomTip"
-      />
-      <StandardTip
-        tip="15"
-        v-model="standardTip"
-        @clear-custom-tip="clearCustomTip"
-      />
-      <StandardTip
-        tip="25"
-        v-model="standardTip"
-        @clear-custom-tip="clearCustomTip"
-      />
-      <StandardTip
-        tip="50"
-        v-model="standardTip"
-        @clear-custom-tip="clearCustomTip"
-      />
-      <input
-        type="text"
-        placeholder="Custom"
-        id="custom"
-        class="bg-theme-very-light-grayish-cyan text-theme-very-dark-cyan placeholder:text-theme-dark-grayish-cyan focus:bg-theme-very-light-grayish-cyan focus:outline-theme-strong-cyan caret-theme-strong-cyan rounded p-2 text-center text-2xl hover:cursor-pointer focus:text-right focus:placeholder:opacity-0"
-        v-model="customTip"
-      />
+      <CustomTip v-model="customTip" />
     </div>
   </div>
 </template>
