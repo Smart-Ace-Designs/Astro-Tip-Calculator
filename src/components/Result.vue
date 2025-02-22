00ae73ae8d356e0c0528dc14b592e24ef5867a60
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-02-22 10:38:21 -0600

    Moves results to separate Vue component

---
+++ src/components/Result.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <div
+    class="bg-theme-very-dark-cyan my-8 mr-10 flex flex-col space-y-10 rounded-2xl px-10 pt-12 pb-8"
+  >
+    <div class="flex justify-between">
+      <div>
+        <h2 class="text-theme-light-grayish-cyan">Tip Amount</h2>
+        <p class="text-theme-grayish-cyan text-sm">/ person</p>
+      </div>
+      <p class="text-theme-strong-cyan text-5xl">$0.00</p>
+    </div>
+    <div class="flex justify-between">
+      <div>
+        <h2 class="text-theme-light-grayish-cyan">Total</h2>
+        <p class="text-theme-grayish-cyan text-sm">/ person</p>
+      </div>
+      <p class="text-theme-strong-cyan text-5xl">$0.00</p>
+    </div>
+    <div class="mt-auto"></div>
+    <button
+      class="bg-theme-bath-green text-theme-very-dark-cyan/30 hover:bg-theme-light-grayish-cyan hover:text-theme-very-dark-cyan w-full rounded-md py-2.5 text-xl uppercase"
+    >
+      Reset
+    </button>
+  </div>
+</template>
