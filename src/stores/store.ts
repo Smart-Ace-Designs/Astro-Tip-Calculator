ddf6fb89c5b829104ad45c0264b30b8a61def39d
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-03-03 18:50:28 -0600

    Fixes initial tip value

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,5 +1,5 @@
 import { atom } from "nanostores";
 
-export const tip = atom<string>("");
+export const tip = atom<string>("15");
 export const bill = atom<string>("");
 export const party = atom<string>("");
