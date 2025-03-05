2e02ef4c5e2e6eafaeb05e5a5edb37acf7931f64
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-03-05 12:53:07 -0600

    Converts to arrow function

--- src/composables/useComputedStore.ts
+++ src/composables/useComputedStore.ts
@@ -2,10 +2,10 @@ import { computed } from "vue";
 import type { WritableAtom } from "nanostores";
 import { useStore } from "@nanostores/vue";
 
-export function useComputedStore(atom: WritableAtom) {
+export const useComputedStore = (atom: WritableAtom) => {
   const store = useStore(atom);
   return computed({
     get: () => store.value,
     set: (value) => atom.set(value),
   });
-}
+};
