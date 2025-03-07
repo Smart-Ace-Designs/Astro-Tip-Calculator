a9d91d528abf2024c082098ab079a0342e4b8e11
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-03-06 18:03:01 -0600

    Fixes invalid custom tip values

--- src/components/CustomTip.vue
+++ src/components/CustomTip.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { tip } from "@/stores/store";
+import { ref } from "vue";
 
 const emit = defineEmits(["clear-standard-tip", "test-custom-tip"]);
 const model = defineModel<string>();
+const invalidCustomTip = ref<boolean>(false);
 
-const updateTip = (event: Event) => {
-  const value = (event.target as HTMLInputElement).value;
-  tip.set(value);
+const updateTip = (event: Event): void => {
+  const value = Number((event.target as HTMLInputElement).value);
+  if (Number.isInteger(value) && value > 0 && value <= 100) {
+    tip.set(value.toString());
+    invalidCustomTip.value = false;
+  } else {
+    tip.set("");
+    invalidCustomTip.value = true;
+  }
 };
 </script>
 
@@ -19,6 +27,8 @@ const updateTip = (event: Event) => {
     type="text"
     placeholder="Custom"
     id="custom"
+    maxlength="3"
     class="bg-theme-very-light-grayish-cyan text-theme-very-dark-cyan placeholder:text-theme-dark-grayish-cyan focus:bg-theme-very-light-grayish-cyan focus:outline-theme-strong-cyan caret-theme-strong-cyan rounded p-2 text-center text-2xl hover:cursor-pointer focus:text-right focus:placeholder:opacity-0"
+    :class="{ '!text-red-400 underline': invalidCustomTip }"
   />
 </template>
